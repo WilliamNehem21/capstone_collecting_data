f7aae5e8816f02a39c63ae008e8b4e82f1ef5abb
WilliamNehem21 <106363970+WilliamNehem21@users.noreply.github.com>
2023-06-13 16:45:27 +0700

    Update scraping_images_new.py

--- kode preprocessing junk food/scraping_code/scraping_images_new.py
+++ kode preprocessing junk food/scraping_code/scraping_images_new.py
@@ -48,29 +48,26 @@ def get_suggested_search_data(paramsIn):
 
     all_script_tags = soup.select("script")
 
-    # https://regex101.com/r/48UZhY/6
+    
     matched_images = "".join(re.findall(r"AF_initDataCallback\(({key: 'ds:1'.*?)\);</script>", str(all_script_tags)))
     
-    # https://kodlogs.com/34776/json-decoder-jsondecodeerror-expecting-property-name-enclosed-in-double-quotes
-    # if you try to json.loads() without json.dumps it will throw an error:
-    # "Expecting property name enclosed in double quotes"
+    
     matched_images_data_fix = json.dumps(matched_images)
     matched_images_data_json = json.loads(matched_images_data_fix)
 
-    # search for only suggested search thumbnails related
-    # https://regex101.com/r/ITluak/2
+    
     suggested_search_thumbnails = ",".join(re.findall(r'{key(.*?)\[null,\"Size\"', matched_images_data_json))
 
-    # https://regex101.com/r/MyNLUk/1
+    
     suggested_search_thumbnail_encoded = re.findall(r'\"(https:\/\/encrypted.*?)\"', suggested_search_thumbnails)
 
     for suggested_search, suggested_search_fixed_thumbnail in zip(soup.select(".PKhmud.sc-it.tzVsfd"), suggested_search_thumbnail_encoded):
         suggested_searches.append({
             "name": suggested_search.select_one(".VlHyHc").text,
             "link": f"https://www.google.com{suggested_search.a['href']}",
-            # https://regex101.com/r/y51ZoC/1
+            
             "chips": "".join(re.findall(r"&chips=(.*?)&", suggested_search.a["href"])),
-            # https://stackoverflow.com/a/4004439/15164646 comment by Frédéric Hamidi
+            
             "thumbnail": bytes(suggested_search_fixed_thumbnail, "ascii").decode("unicode-escape")
         })
 
@@ -78,26 +75,22 @@ def get_suggested_search_data(paramsIn):
 
 def get_original_images(folder_name, menu):
 
-    """
-    https://kodlogs.com/34776/json-decoder-jsondecodeerror-expecting-property-name-enclosed-in-double-quotes
-    if you try to json.loads() without json.dumps() it will throw an error:
-    "Expecting property name enclosed in double quotes"
-    """
+   
 
     google_images = []
 
     all_script_tags = soup.select("script")
 
-    # # https://regex101.com/r/48UZhY/4
+    
     matched_images_data = "".join(re.findall(r"AF_initDataCallback\(([^<]+)\);", str(all_script_tags)))
     
     matched_images_data_fix = json.dumps(matched_images_data)
     matched_images_data_json = json.loads(matched_images_data_fix)
 
-    # https://regex101.com/r/VPz7f2/1
+    
     matched_google_image_data = re.findall(r'\"b-GRID_STATE0\"(.*)sideChannel:\s?{}}', matched_images_data_json)
 
-    # https://regex101.com/r/NnRg27/1
+    
     matched_google_images_thumbnails = ", ".join(
         re.findall(r'\[\"(https\:\/\/encrypted-tbn0\.gstatic\.com\/images\?.*?)\",\d+,\d+\]',
                    str(matched_google_image_data))).split(", ")
@@ -106,12 +99,11 @@ def get_original_images(folder_name, menu):
         bytes(bytes(thumbnail, "ascii").decode("unicode-escape"), "ascii").decode("unicode-escape") for thumbnail in matched_google_images_thumbnails
     ]
 
-    # removing previously matched thumbnails for easier full resolution image matches.
+    
     removed_matched_google_images_thumbnails = re.sub(
         r'\[\"(https\:\/\/encrypted-tbn0\.gstatic\.com\/images\?.*?)\",\d+,\d+\]', "", str(matched_google_image_data))
 
-    # https://regex101.com/r/fXjfb1/4
-    # https://stackoverflow.com/a/19821774/15164646
+   
     matched_google_full_resolution_images = re.findall(r"(?:'|,),\[\"(https:|http.*?)\",\d+,\d+\]", removed_matched_google_images_thumbnails)
 
     full_res_images = [
